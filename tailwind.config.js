00c3e681e824a31492a46d513ac563edc8e991d2
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-05-31 07:11:54 +0000

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,7 @@ export default {
     plugins: [
         plugin(function ({ addBase, addComponents, addUtilities }) {
             addBase({});
+
             addComponents({
                 ".container": {
                     "@apply max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem]":
