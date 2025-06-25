b9c0b9c512db7d236ff8f4ddb3ded46d98a70ac8
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-25 18:35:59 +0530

    203

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import ButtonGradient from './assets/svg/ButtonGradient';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
