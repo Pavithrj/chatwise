e1218b52e4fa08c7735b298848307cf9cceeff7a
Pavithr Jain <pavithrjain19@gmail.com>
2025-06-25 21:37:14 +0530

    208

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,6 @@
 import Section from './Section';
 import Heading from './Heading';
 import Generating from './Generating';
-import { service1, service2, service3, check } from './../assets';
 import { PhotoChatMessage, Gradient, VideoBar, VideoChatMessage } from './design/Services';
 
 const Services = () => {
