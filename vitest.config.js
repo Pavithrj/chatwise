7be1394a6b7c2545d372423870d686577dda93b4
Pavithr Jain <pavithrjain19@gmail.com>
2025-09-03 19:45:37 +0530

    286

---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    test: {
+        environment: "jsdom"
+    },
+    plugins: [
+        react({
+            jsxRuntime: "automatic"
+        })
+    ]
+});
